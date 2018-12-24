01c6e5c40dad85e379145032189dc9c210b1c6c1
Yessine Borchani <borchaniz96@gmail.com>
2018-12-24 23:26:44 +0100

    minor changes

--- src/app/shared/models/discount.ts
+++ src/app/shared/models/discount.ts
@@ -2,4 +2,9 @@ export class Discount {
   percentage: number;
   startDate: Date;
   endDate: Date;
+
+  isValid() {
+    return this.percentage && this.percentage > 0 && this.percentage < 100
+      && this.startDate && this.endDate && this.startDate < this.endDate;
+  }
 }
